93d7f94152be9dfca4ffd9aa2050441d90e9f231
Daria Pantiushina <the1007pan.da@gmail.com>
2025-10-19 20:55:27 +0200

    Add files via upload

--- sql/01_stg.sql
+++ sql/01_stg.sql
@@ -1,20 +1,18 @@
-CREATE TABLE transactions (
+CREATE TABLE stg.transactions (
     txn_id VARCHAR(20),
     user_id VARCHAR(20),
     amount NUMERIC(10,2),
     currency CHAR(3),
     txn_date TIMESTAMP,
     status VARCHAR(20),
-    provider VARCHAR(50),
-    load_dttm TIMESTAMP DEFAULT now()
+    provider VARCHAR(50)
 );
 
-CREATE TABLE chargebacks (
+CREATE TABLE stg.chargebacks (
     cb_id VARCHAR(20),
     txn_id VARCHAR(20),
     cb_date TIMESTAMP,
-    reason VARCHAR(100),
-    load_dttm TIMESTAMP DEFAULT now()
+    reason VARCHAR(100)
 );
 
 --############################################################--
