e7faf857e5bcc16cba4d9a82d2c26e40a5bd998e
Daria Pantiushina <the1007pan.da@gmail.com>
2025-10-19 19:54:47 +0200

    Add files via upload

--- sql/01_stg.sql
+++ sql/01_stg.sql
@@ -1,18 +1,24 @@
-CREATE TABLE stg.stg_transactions (
-    txn_id VARCHAR(20),
-    user_id VARCHAR(20),
-    amount NUMERIC(10,2),
-    currency CHAR(3),
-    txn_date TIMESTAMP,
-    status VARCHAR(20),
-    provider VARCHAR(50),
-    load_dttm TIMESTAMP DEFAULT now()
-);
+DROP TABLE IF EXISTS stg.stg_transactions;
+CREATE TABLE stg.stg_transactions AS
+SELECT DISTINCT
+    txn_id,
+    user_id,
+    CAST(amount AS NUMERIC(10,2)) AS amount,
+    UPPER(currency) AS currency,
+    CAST(txn_date AS TIMESTAMP) AS txn_date,
+    status,
+    provider
+FROM transactions
+WHERE status IN ('SUCCESS', 'FAILED', 'REFUNDED')
+  AND amount > 0
+  AND currency IS NOT NULL;
 
-CREATE TABLE stg.stg_chargebacks (
-    cb_id VARCHAR(20),
-    txn_id VARCHAR(20),
-    cb_date TIMESTAMP,
-    reason VARCHAR(100),
-    load_dttm TIMESTAMP DEFAULT now()
-);
+DROP TABLE IF EXISTS stg.stg_chargebacks;
+CREATE TABLE stg.stg_chargebacks AS
+SELECT DISTINCT
+    cb_id,
+    txn_id,
+    CAST(cb_date AS TIMESTAMP) AS cb_date,
+    TRIM(reason) AS reason
+FROM chargebacks
+WHERE cb_id IS NOT NULL AND txn_id IS NOT NULL;
